88242a03cbfc4c7456a05d4b4815c0e51fac1985
Jim Pivarski <jpivarski@gmail.com>
2023-12-27 13:22:23 -0600

    feat: add constants.

---
+++ src/ragged/common/_const.py
@@ -0,0 +1,11 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/ragged/blob/main/LICENSE
+
+"""
+https://data-apis.org/array-api/latest/API_specification/constants.html
+"""
+
+from __future__ import annotations
+
+from numpy import e, inf, nan, newaxis, pi
+
+__all__ = ["e", "inf", "nan", "newaxis", "pi"]
