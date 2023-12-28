054d23c0ecc1700719a3bd4fa1c5edc2cb727d4e
Jim Pivarski <jpivarski@gmail.com>
2023-12-28 06:35:32 -0600

    Stubbed all remaining functions in ragged.common.

--- src/ragged/common/_search.py
+++ src/ragged/common/_search.py
@@ -6,7 +6,113 @@ https://data-apis.org/array-api/latest/API_specification/searching_functions.htm
 
 from __future__ import annotations
 
-argmax = None
-argmin = None
-nonzero = None
-where = None
+from ._obj import array
+
+
+def argmax(x: array, /, *, axis: None | int = None, keepdims: bool = False) -> array:
+    """
+    Returns the indices of the maximum values along a specified axis.
+
+    When the maximum value occurs multiple times, only the indices
+    corresponding to the first occurrence are returned.
+
+    Args:
+        x: Input array.
+        axis: Axis along which to search. If `None`, the function returns the
+            index of the maximum value of the flattened array.
+        keepdims: If `True`, the reduced axes (dimensions) are included in the
+            result as singleton dimensions, and, accordingly, the result is
+            broadcastable with the input array. Otherwise, if `False`, the
+            reduced axes (dimensions) are not included in the result.
+
+    Returns:
+        If `axis` is `None`, a zero-dimensional array containing the index of
+        the first occurrence of the maximum value; otherwise, a
+        non-zero-dimensional array containing the indices of the maximum
+        values. The returned array has data type `np.int64`.
+
+    https://data-apis.org/array-api/latest/API_specification/generated/array_api.argmax.html
+    """
+
+    assert x, "TODO"
+    assert axis, "TODO"
+    assert keepdims, "TODO"
+    assert False, "TODO"
+
+
+def argmin(x: array, /, *, axis: None | int = None, keepdims: bool = False) -> array:
+    """
+    Returns the indices of the minimum values along a specified axis.
+
+    When the minimum value occurs multiple times, only the indices
+    corresponding to the first occurrence are returned.
+
+    Args:
+        x: Input array.
+        axis: Axis along which to search. If `None`, the function returns the
+            index of the minimum value of the flattened array.
+        keepdims: If `True`, the reduced axes (dimensions) are included in the
+            result as singleton dimensions, and, accordingly, the result is
+            broadcastable with the input array. Otherwise, if `False`, the
+            reduced axes (dimensions) are not included in the result.
+
+    Returns:
+        If `axis` is `None`, a zero-dimensional array containing the index of
+        the first occurrence of the minimum value; otherwise, a
+        non-zero-dimensional array containing the indices of the minimum
+        values. The returned array has data type `np.int64`.
+
+    https://data-apis.org/array-api/latest/API_specification/generated/array_api.argmin.html
+    """
+
+    assert x, "TODO"
+    assert axis, "TODO"
+    assert keepdims, "TODO"
+    assert False, "TODO"
+
+
+def nonzero(x: array, /) -> tuple[array, ...]:
+    """
+    Returns the indices of the array elements which are non-zero.
+
+    Args:
+        x: Input array. Must have a positive rank. If `x` is zero-dimensional,
+        the function raises an exception.
+
+    Returns:
+        A tuple of `k` arrays, one for each dimension of `x` and each of size
+        `n` (where `n` is the total number of non-zero elements), containing
+        the indices of the non-zero elements in that dimension. The indices
+        are returned in row-major, C-style order. The returned array has data
+        type `np.int64`.
+
+    https://data-apis.org/array-api/latest/API_specification/generated/array_api.nonzero.html
+    """
+
+    assert x, "TODO"
+    assert False, "TODO"
+
+
+def where(condition: array, x1: array, x2: array, /) -> array:
+    """
+    Returns elements chosen from `x1` or `x2` depending on `condition`.
+
+    Args:
+        condition: When `True`, yield `x1_i`; otherwise, yield `x2_i`. Must be
+            broadcastable with `x1` and `x2`.
+        x1: First input array. Must be broadcastable with `condition` and `x2`.
+        x2: Second input array. Must be broadcastable with `condition` and
+            `x1`.
+
+    Returns:
+        An array with elements from `x1` where condition is `True`, and
+        elements from `x2` elsewhere. The returned array has a data type
+        determined by type promotion rules with the arrays `x1` and `x2`.
+
+    https://data-apis.org/array-api/latest/API_specification/generated/array_api.where.html
+    """
+
+    assert condition, "TODO"
+    assert x1, "TODO"
+    assert x2, "TODO"
+    assert False, "TODO"
