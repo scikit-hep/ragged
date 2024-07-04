e80438f9934ecbaf97146453feaeb930294cf1ae
Oleksii Hrechykha <alexeee6@gmail.com>
2024-07-04 10:50:31 +0300

    adding worded algorithm for unique_values in test_spec_set_functions.py

--- tests/test_spec_set_functions.py
+++ tests/test_spec_set_functions.py
@@ -8,6 +8,13 @@ from __future__ import annotations
 
 import ragged
 
+#Specific algorithm for unique_values:
+#1 take an input array
+#2 flatten input_array unless its 1d
+#3 {remember the first element, loop through the rest of the list to see if there are copies
+#    if yes then discard it and repeat the step
+#    if not then add it to the output and repeat the step}
+#4 once the cycle is over return an array of unique elements in the input array (the output must be of the same type as input array)
 
 def test_existence():
     assert ragged.unique_all is not None
