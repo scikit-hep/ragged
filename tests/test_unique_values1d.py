c0693c280b00715c54b0a3bff3e138e5491e67c5
Oleksii Hrechykha <alexeee6@gmail.com>
2024-07-08 10:11:27 +0300

    changing ak to ragged in test_unique_values1d

--- tests/test_unique_values1d.py
+++ tests/test_unique_values1d.py
@@ -1,6 +1,7 @@
+from unique_values1d import unique_values1d
 import awkward as ak
 import ragged
-from unique_values1d import unique_values1d
+
 def test_can_take_none():
     assert unique_values1d(None)==None
 
@@ -8,15 +9,15 @@ def test_can_take_list():
     assert unique_values1d([1,2,4,3,4,5,6,20])
 
 def test_can_take_0():
-    assert unique_values1d(ak.Array([0]))==ak.Array([0])
+    assert unique_values1d(ragged.array([0]))==ragged.array([0])
 
 def test_can_take_empty_arr():
-    assert unique_values1d(ak.Array())
+    assert unique_values1d(ragged.array())
 
 def test_can_take_moredimensions():
-    assert unique_values1d(ak.Array([1,2,3,4,[5,6]]))
+    assert unique_values1d(ragged.array([1,2,3,4,[5,6]]))
 
 def test_can_take_1d_array():
-    assert unique_values1d(ak.Array([5,6,7,8,8,9,1,2,3,4,10,0,15,2]))==ak.Array([0,1,2,3,4,5,6,7,8,9,10,15])
+    assert unique_values1d(ragged.array([5,6,7,8,8,9,1,2,3,4,10,0,15,2]))==ragged.array([0,1,2,3,4,5,6,7,8,9,10,15])
 
 
