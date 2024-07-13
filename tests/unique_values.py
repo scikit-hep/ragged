f40dc74e424e9c418aa111c94235acf73421d7a9
Oleksii Hrechykha <alexeee6@gmail.com>
2024-07-13 07:58:01 +0300

    adding ak.ravel to unique values function

--- tests/unique_values.py
+++ tests/unique_values.py
@@ -5,14 +5,12 @@ import ragged
 def unique_values1d(arr):
     if not isinstance(arr, ragged.array):
         print("Input is not a ragged array")
-        
-    if arr.ndim != 1:
-        print("Input is not a 1D array")
 
     if len(arr)==1:
         return arr
 
-    arr_np = ak.to_numpy(arr)
+    arr_flat=ak.ravel(arr)
+    arr_np = ak.to_numpy(arr_flat)
     unique_np = np.unique(arr_np)
     unique_arr = ak.from_numpy(unique_np)
 
