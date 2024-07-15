203124bf3796a5ab959a7187da8cf01db50126a6
Oleksii Hrechykha <alexeee6@gmail.com>
2024-07-15 13:53:17 +0300

    adding inverse and counts functions written via numpy

---
+++ tests/unique_counts.py
@@ -0,0 +1,13 @@
+def unique_counts(arr):
+    if not isinstance(arr, ragged.array):
+        print("Input is not a ragged array")
+
+    if len(arr)==1:
+        return [(arr[0], 1)]
+
+    arr_flat=ak.ravel(arr)
+    arr_np = ak.to_numpy(arr_flat)
+    unique_elements, counts = np.unique(arr_np, return_counts=True)
+    unique_arr_counts = list(zip(unique_elements, counts))
+
+    return unique_arr_counts
